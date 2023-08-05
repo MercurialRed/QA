908dbad335ce3e8e9bf20752fe2d8ec0c9c0aec7
MercurialRed <mercurialred54@gmail.com>
2023-08-05 17:01:13 +0300

    update elements_test.py added test radio button

--- Python_autotests/pages/elements_page.py
+++ Python_autotests/pages/elements_page.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.by import By
 
 from Python_autotests.generator.generator import generated_person
 from Python_autotests.pages.base_page import BasePage
-from Python_autotests.locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from Python_autotests.locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, \
+    RadioButtonPageLocators
 
 
 class TextBoxPage(BasePage):
@@ -33,6 +34,7 @@ class TextBoxPage(BasePage):
         permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
 
+
 class CheckBoxPage(BasePage):
     locators = CheckBoxPageLocators()
 
@@ -48,7 +50,7 @@ class CheckBoxPage(BasePage):
                 self.go_to_element(item)
                 item.click()
                 print(item.text)
-                count -=1
+                count -= 1
             else:
                 break
 
@@ -68,3 +70,16 @@ class CheckBoxPage(BasePage):
             data.append(item.text)
         print(data)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_radio_button(self, choice):
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                    'no': self.locators.NO_RADIOBUTTON}
+        self.element_is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
