8608e1800df7cc60743048a27a2039ea83f007bb
MercurialRed <mercurialred54@gmail.com>
2023-08-23 15:35:44 +0300

    added class:    - TestFramesPage    - TestNestedFramesPage    - TestModalDialogsPage added tests:    - test_frames    - test_nested_frames    - test_modal_dialogs

--- Python_autotests/tests/alerts_frame_windows_test.py
+++ Python_autotests/tests/alerts_frame_windows_test.py
@@ -1,4 +1,5 @@
-from Python_autotests.pages.alerts_frame_windows_page import BrowserWindowsPage
+from Python_autotests.pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage, \
+    NestedFramesPage, ModalDialogsPage
 
 
 class TestAlertsFrameWindow:
@@ -15,3 +16,60 @@ class TestAlertsFrameWindow:
             browser_windows_page.open()
             text_result = browser_windows_page.check_opened_new_window()
             assert text_result == "This is a sample page", "The new window has not opened or an incorrect window has opened"
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts/")
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == "You clicked a button", "Alert did not show up"
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts/")
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == "This alert appeared after 5 seconds", "Alert did not show up"
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts/")
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == "You selected Ok", "Alert did not show up"
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts/")
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, "Alert did not show up"
+
+    class TestFramesPage:
+
+        def test_frames(self, driver):
+            frame_page = FramesPage(driver, "https://demoqa.com/frames")
+            frame_page.open()
+            result_frame1 = frame_page.check_frame('frame1')
+            result_frame2 = frame_page.check_frame('frame2')
+            assert result_frame1 == ['This is a sample page', '500px', '350px'], "The frame does not exist"
+            assert result_frame2 == ['This is a sample page', '100px', '100px'], "The frame does not exist"
+
+    class TestNestedFramesPage:
+
+        def test_nested_frames(self, driver):
+            nested_frame_page = NestedFramesPage(driver, "https://demoqa.com/nestedframes")
+            nested_frame_page.open()
+            parent_text, child_text = nested_frame_page.check_nested_frame()
+            assert parent_text == 'Parent frame', "Nested frame does not exist"
+            assert child_text == 'Child frame', "Nested frame does not exist"
+
+    class TestModalDialogsPage:
+
+        def test_modal_dialogs(self, driver):
+            modal_dialogs_page = ModalDialogsPage(driver, "https://demoqa.com/modal-dialogs")
+            modal_dialogs_page.open()
+            small, large = modal_dialogs_page.check_modal_button()
+            print(small)
+            print(large)
+            assert small[1] < large[1], "Text from the large dialog is less than from the small dialog"
+            assert small[0] == "Small Modal", 'The header is not "Small modal"'
+            assert large[0] == "Large Modal", 'The header is not "Large modal"'
